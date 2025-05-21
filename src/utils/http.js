dcc474c9179d52d8d5d1031baf758832ea72b5ab
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-05-21 14:40:31 +0500

    function for creating new event is written, can be used using useMutation but full functionality remains, such that for image selecttion

--- src/utils/http.js
+++ src/utils/http.js
@@ -17,3 +17,22 @@ export async function fetchEvents({ search, signal }) {
 
   return events;
 }
+
+export async function createNewEvent(eventData) {
+  const response = await fetch("http://localhost:3000/events", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(eventData),
+  });
+
+  if (!response.ok) {
+    const error = new Error("An error occurred while creating the events");
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
+}
